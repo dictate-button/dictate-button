ef8681a81f5dc17fca19adf583b8d01077e36b8e
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-07-21 22:03:24 +0200

    Added custom 404 page.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import solidPlugin from "vite-plugin-solid";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
@@ -8,15 +9,23 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html',
+          dest: '',
+        },
+      ],
+    }),
   ],
   build: {
     lib: {
       entry: {
-        "dictate-button": "src/dictate-button.tsx",
-        "inject": "src/inject.js"
+        'dictate-button': 'src/dictate-button.tsx',
+        inject: 'src/inject.js',
       },
-      formats: ["es", "cjs"],
+      formats: ['es', 'cjs'],
       fileName: (format, entryName) => `${entryName}.${format}.js`,
     },
   },
-});
+})
