aace6b7607b6bea8d0c86868d3026a17ba8f3d19
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-08-19 14:51:05 +0200

    fix: Had to hardcode the output js file extension.

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
         'inject-inclusive': 'src/inject-inclusive.js',
       },
       formats: ['es'],
+      fileName: (format, entryName) => `${entryName}.mjs`,
     },
   },
 })
