c8c3a72e3e195435190027485c20be0afe8f59b1
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-08-19 14:49:06 +0200

    Reverted type change recommended by coderabbitai to pass linting.

--- src/dictate-button.tsx
+++ src/dictate-button.tsx
@@ -55,7 +55,7 @@ customElement(
     // Audio analysis variables
     let audioCtx: AudioContext | null = null
     let analyser: AnalyserNode | null = null
-    let dataArray: Uint8Array | null = null
+    let dataArray: Uint8Array<ArrayBuffer> | null = null
     let running = false
     let smoothLevel = 0
 
@@ -146,7 +146,9 @@ customElement(
           analyser = audioCtx.createAnalyser()
           analyser.fftSize = 2048
           source.connect(analyser)
-          dataArray = new Uint8Array(analyser.fftSize)
+          dataArray = new Uint8Array(
+            analyser.fftSize
+          ) as Uint8Array<ArrayBuffer>
 
           mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm' })
           audioChunks = []
